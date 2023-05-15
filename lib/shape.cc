2253a275dd1f04efb91145702a3c7f08359e5c35
Minwoo Jeong <jeongmwj@gmail.com>
2023-05-15 21:50:47 +0900

    Create a library example

---
+++ lib/shape.cc
@@ -0,0 +1,5 @@
+#include "shape.h"
+
+Rectangle::Rectangle(int width, int height) : width_{width}, height_{height} {}
+
+int Rectangle::GetSize() const { return width_ * height_; }
--- main.cc
+++ main.cc
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include "shape.h"
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+  Rectangle rect{4, 8};
+  std::cout << "Hello, world!" << std::endl;
+  std::cout << "Rectangle size: " << rect.GetSize() << std::endl;
 }
